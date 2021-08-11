5719a23d0c36f109e8f9008367c0256716aebd68
Marcos <marcosnicolau2003@gmailcom>
2021-08-11 19:37:05 -0300

    feat: fire deploy if tag is latest

--- controllers/deploy.js
+++ controllers/deploy.js
@@ -1,18 +1,20 @@
-const shell = require('shelljs');
-const dotenv = require('dotenv');
+const shell = require("shelljs");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const deployHook = (req, res) => {
+	const { push_data } = req.body;
 	const { token, script } = req.query;
-	if (!token) return res.status(401).send('missing token');
+	//We don't want to fire deploy if the image tag is no latest
+	if (push_data.tag !== "latest") return res.send("not latest tag");
+	if (!token) return res.status(401).send("missing token");
 	if (token !== process.env.PIPELINE_TOKEN) {
-		console.log('wrong token');
-		return res.status(401).send('wrong token');
+		console.log("wrong token");
+		return res.status(401).send("wrong token");
 	}
-	res.send('success');
-	console.log(req.body);
-	shell.chmod('+x', `/scripts/${script || 'deploy'}.sh`);
-	shell.exec(`/scripts/${script || 'deploy'}.sh`, { async: true });
+	res.send("success");
+	shell.chmod("+x", `/scripts/${script || "deploy"}.sh`);
+	shell.exec(`/scripts/${script || "deploy"}.sh`, { async: true });
 };
 
 module.exports = {
