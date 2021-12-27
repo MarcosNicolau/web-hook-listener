3504dc7831a3ff869f49df746376aa1d1872453b
Nicolau <marcos@exchangecopter.com>
2021-12-27 11:29:47 -0300

    binding port for docker

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const transporter = require('./config/transporter');
-const { deployHook } = require('./controllers/deploy');
-const { mailDeploy } = require('./controllers/mail');
+const express = require("express");
+const dotenv = require("dotenv");
+const transporter = require("./config/transporter");
+const { deployHook } = require("./controllers/deploy");
+const { mailDeploy } = require("./controllers/mail");
 dotenv.config();
+
+const PORT = 3000;
 
 const app = express();
 app.use(express.json());
 
-const deployRoute = process.env.PIPELINE_DEPLOY_ROUTE || '/deploy';
+const deployRoute = process.env.PIPELINE_DEPLOY_ROUTE || "/deploy";
 
 app.post(`${deployRoute}/mail`, mailDeploy);
 app.post(deployRoute, deployHook);
 
-app.listen('3000');
+app.listen(Number(PORT), "0.0.0.0", () => console.log(`server listening on port ${PORT}`));
